9193245c5760f5c6e405acb36af13e9ff08e6449
DESKTOP-24GU9HH\ASUS <lekhaduy0917880045@gmail.com>
2024-03-25 02:13:25 +0700

    insert

--- src/app.js
+++ src/app.js
@@ -4,10 +4,14 @@ const express = require('express')
 const path = require('path')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
+const connection = require('./config/database')
 
 const app = express()
 const port = process.env.PORT || 8888
 const hostname = process.env.hostname
+
+app.use(express.json()); // Used to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 
 configViewEngine(app)
 
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,27 @@
+const connection = require('../config/database');
 
 const getHomepage = (req, res) => {
-    res.send('This is homepage')
+    return res.render('homepage.ejs');
 }
 
+const postCreateUser = (req, res) => {
+    // let{name, email, city} = req.body;
+    let name = req.body.name;
+    let email = req.body.email;
+    let city = req.body.city;
+
+    connection.query(
+        `INSERT INTO Users (email, name, city)
+        VALUES (?, ?, ?)`,
+        [email, name, city],
+        function (err, results) {
+            console.log(results);
+            res.send('Success');
+        }
+    );
+};
+
 module.exports = {
-    getHomepage
+    getHomepage,
+    postCreateUser
 }
